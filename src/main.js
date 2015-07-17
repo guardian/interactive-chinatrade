4c204597b6c5d5e366db526fe964271e4d715adf
seanclarkeguardian <sean.clarke@guardian.co.uk>
2015-07-17 10:39:12 +0100

    working fri am

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ function drawtopcountriesgraph(topcountries) {
 	var width = 420, barHeight = 20;
 
 	var x = d3.scale.linear()
-		.domain([0, d3.max(topcountries,function(d){return d.chinaexportsovergdp})])
+		.domain([0, d3.max(topcountries,function(d){return d.chinaexports})])
 		.range([0, width]);
 
 	var chart = d3.select('.chart')
@@ -25,18 +25,18 @@ function drawtopcountriesgraph(topcountries) {
 		.attr('height', barHeight * topcountries.length);
 
 	var bar = chart.selectAll('g')
-		.data(topcountries.sort(function(a,b){return b.chinaexportsovergdp-a.chinaexportsovergdp}))
+		.data(topcountries.sort(function(a,b){return b.chinaexports-a.chinaexports}))
 		.enter().append('g')
 		.attr("transform", function (d, i) { return "translate(0," + i * barHeight + ")"; });
 
 	bar.append("rect")
 		.attr("width", function(d){
-			return x(d.chinaexportsovergdp);
+			return x(d.chinaexports);
 		})
 		.attr("height", barHeight - 1);
 
 	bar.append("text")
-		.attr("x", function (d) { return x(d.chinaexportsovergdp) - 3; })
+		.attr("x", function (d) { return x(d.chinaexports) - 3; })
 		.attr("y", barHeight / 2)
 		.attr("dy", ".35em")
 		.text(function (d) { return d.country; });
